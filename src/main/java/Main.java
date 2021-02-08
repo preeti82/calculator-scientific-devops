df36d873198df82f232bde80f396dec84a6ed508
preeti <preeti.singhal@iiitb.org>
2021-02-09 00:59:48 +0530

    Adding square root functionality

---
+++ src/main/java/Main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Welcome to scientific calculator program");
+        System.out.println("Press 1 to compute square root, Press 2 to compute factorial, Press 3 to compute natural log, Press 4 to compute power function, Press any other key to exit");
+        System.out.print("Enter your choice: ");
+        Scanner scanner = new Scanner(System.in);
+        try {
+            int choice = scanner.nextInt();
+            if (choice == 1){
+                System.out.print("Please enter a number on which you want to compute square root: ");
+                double number = scanner.nextDouble();
+                System.out.println("Your result is "+ Math.sqrt(number));
+            }
+        }catch (Exception e){
+            System.out.println("Exiting.....");
+        }
+    }
+}
