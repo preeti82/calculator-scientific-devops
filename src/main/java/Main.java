267eace374fee7101fe4326ed218c80c13199c1e
preeti <preeti.singhal@iiitb.org>
2021-03-11 23:28:28 +0530

    changes

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -13,54 +13,53 @@ public class Main {
 
 
         try {
-                    int choice = scanner.nextInt();
+            int choice = scanner.nextInt();
 
-                    if (choice == 1) {
-                        System.out.print("Please enter a number on which you want to compute square root: ");
-                        double number = scanner.nextDouble();
-                        System.out.println("Your result is " + new Main().squareRoot(number));
+            if (choice == 1) {
+                System.out.print("Please enter a number on which you want to compute square root: ");
+                double number = scanner.nextDouble();
+                System.out.println("Your result is " + new Main().squareRoot(number));
 
-                    } else if (choice == 2) {
-                        System.out.print("Please enter a number on which you want to compute Factorial: ");
-                        double number = scanner.nextDouble();
-                        System.out.println("Factorial of " + number + " is: " + new Main().factorial(number));
-                    } else if (choice == 3) {
-                        System.out.print("Please enter a number on which you want to compute Natural log: ");
-                        double number = scanner.nextDouble();
-                        System.out.println("Your result is " + new Main().logFunction(number));
-                    } else if (choice == 4) {
-                        System.out.print("Please enter a number on which you want to compute Power Function: ");
-                        double number1 = scanner.nextDouble();
-                        double number2 = scanner.nextDouble();
-                        System.out.println("Your result is " + new Main().powerFunction(number1, number2));
+            } else if (choice == 2) {
+                System.out.print("Please enter a number on which you want to compute Factorial: ");
+                double number = scanner.nextDouble();
+                System.out.println("Factorial of " + number + " is: " + new Main().factorial(number));
+            } else if (choice == 3) {
+                System.out.print("Please enter a number on which you want to compute Natural log: ");
+                double number = scanner.nextDouble();
+                System.out.println("Your result is " + new Main().logFunction(number));
+            } else if (choice == 4) {
+                System.out.print("Please enter a number on which you want to compute Power Function: ");
+                double number1 = scanner.nextDouble();
+                double number2 = scanner.nextDouble();
+                System.out.println("Your result is " + new Main().powerFunction(number1, number2));
 
-                    }
+            }
 
-        }
-
-        catch (Exception e){
+        } catch (Exception e) {
             System.out.println("Exiting.....");
         }
 
 
+    }
 
-    public double squareRoot(double number){
+    public double squareRoot(double number) {
         return Math.sqrt(number);
     }
 
-    public long factorial(double number){
-        long fact=1;
+    public long factorial(double number) {
+        long fact = 1;
         for (int i = 1; i <= number; i++) {
             fact = fact * i;
         }
         return fact;
     }
 
-    public double logFunction(double number){
+    public double logFunction(double number) {
         return Math.log(number);
     }
 
-    public double powerFunction(double number1, double number2){
-        return Math.pow(number1,number2);
+    public double powerFunction(double number1, double number2) {
+        return Math.pow(number1, number2);
     }
 }
